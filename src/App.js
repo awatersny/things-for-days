7a649edbd813d14a7751699c21ac53954319924a
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-24 10:21:38 -0500

    add boots things

--- src/App.js
+++ src/App.js
@@ -49,44 +49,17 @@ const App = () => {
     },
   ])
 
-  const [bensThings, setBensThings] = useState([
+  const [bootsThings, setBootsThings] = useState([
     {
-      name: 'banana',
+      name: 'boots',
       image:
-        'https://images.unsplash.com/photo-1587132137056-bfbf0166836e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        'https://i.imgur.com/EjfV0Hn.png',
       attributes: [
-        'potassium-rich',
-        'yellow when good',
-        'not a taco',
-        'ring ring ring ring ring ring ring',
+        'brown',
+        'leather',
+        'ropers',
+        'made for walking',
       ],
-    },
-    {
-      name: 'turkey',
-      image:
-        'https://images.unsplash.com/photo-1560011961-4ab41261de01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      attributes: [
-        'such gobbles',
-        'good when stuffed',
-        'not a taco',
-        'why gravy exists',
-      ],
-    },
-    {
-      name: 'taco',
-      image:
-        'https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
-      attributes: [
-        'yummy in my tummy',
-        'salsas may vary',
-        'definitely a taco',
-        'very edible',
-      ],
-    },
-    {
-      name: 'linux',
-      image: 'https://i.imgur.com/3BmfSOA.png',
-      attributes: ['not a taco', 'not Windows', 'not macOS', 'Penguins?'],
     },
   ])
 
